f90edee175a593091fd86f23ea209e69916592a1
Andrii Kapinos <broxbeats@gmail.com>
2025-11-14 18:38:06 +0200

    react-hook-form

--- office/src/pages/AddItem.tsx
+++ office/src/pages/AddItem.tsx
@@ -1,14 +1,23 @@
 import '../index.css';
-import React from 'react';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 import { InputTextarea } from 'primereact/inputtextarea';
 import { InputNumber } from 'primereact/inputnumber';
+import { useForm, type SubmitHandler, Controller } from 'react-hook-form';
+
+type AddProduct = {
+  name: string;
+  description: string;
+  price: number;
+}
 
 export function AddItem() {
-  const [name, setName] =  React.useState<string>('');
-  const [description, setDescription] = React.useState<string>('');
-  const [price, setPrice] = React.useState<number | null>(null);
+  const {
+    control,
+    register,
+    handleSubmit,
+  } = useForm<AddProduct>()
+  const onSubmit: SubmitHandler<AddProduct> = (data) => console.log(data)
   return (
     <div className='flex flex-col gap-4'>
       <div className='card justify-children w-[800px]'>
@@ -17,24 +26,46 @@ export function AddItem() {
         </h1>
         <div className='flex gap-4'>
           <Button label='Cancel' severity='secondary'></Button>
-          <Button label='Add'></Button>
+          <Button onClick={handleSubmit(onSubmit)} label='Add'></Button>
         </div>
       </div>
+
       <div className='card'>
-        <div className='h2'>Product Details</div>
-        <div className='mt-4'>
-          <div>Product Name</div>
-          <InputText value={name} onChange={(e) => setName(e.target.value)} className='w-full' />
-        </div>
-        <div className='mt-4'>
-          <div>Product Description</div>
-          <InputTextarea value={description} onChange={(e) => setDescription(e.target.value)} className='w-full' rows={5} />
-        </div>
-        <div className='h2 mt-4'>Pricing</div>
-        <div className='mt-4'>
-          <div>Price</div>
-          <InputNumber value={price} onValueChange={(e) => setPrice(e.value ?? null)} className='w-full' />
-        </div>
+        <form>
+          <div className='h2'>Product Details</div>
+          <div className='mt-4'>
+            <div>Product Name</div>
+            <InputText {...register("name")} className='w-full' />
+          </div>
+          <div className='mt-4'>
+            <div>Product Description</div>
+            <InputTextarea {...register("description")} className='w-full' rows={5} />
+          </div>
+          <div className='h2 mt-4'>Pricing</div>
+          <div className='mt-4'>
+            <div>Price</div>
+             <Controller
+                name="price"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <div>
+                    <InputNumber
+                      value={field.value}
+                      onValueChange={(e) => field.onChange(e.value)} 
+                      onBlur={field.onBlur}
+                      inputRef={field.ref}
+                      useGrouping={false}
+                      placeholder="Enter a number"
+                      className='w-full'
+                    />
+                    {fieldState.error && (
+                      <p className="text-red-500 text-sm">{fieldState.error.message}</p>
+                    )}
+                  </div>
+                )}
+              />
+          </div>
+        </form>
       </div>
     </div>
   )
--- office/src/pages/index.ts
+++ office/src/pages/index.ts
@@ -1,2 +1,2 @@
-export * from './Home';
 export * from './AddItem';
+export * from './Home';
